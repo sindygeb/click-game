fe11b7be0760f125785a8a653850c8db4a2ff6c6
Sindy <sindy.gebhard@gmail.com>
2019-05-03 17:44:31 -0500

    copied project into new repo
    couldn't get react app to work so created a new one and copied over code

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h2>Avengers Clicky Game!</h2>
+            <p>Click on an Avenger to call them to the fight. But don't click on an Avenger twice, or Thanos will win!</p>
+            <br></br>
+        </div>
+    );
+
+}
+export default Header;
---
+++ src/components/Score.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Score(props) {
+    return <div className="score">{props.children}</div>
+}
+
+export default Score;
